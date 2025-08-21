aac4b8ea000e3b38fbd8c9f57506ccecb8d4734e
elinton-souza <esc.vistorias@gmail.com>
2025-08-21 11:32:28 -0300

    Adicionando construtores

--- Cliente.ts
+++ Cliente.ts
@@ -1,4 +1,13 @@
 export class Cliente {
-    nome: string = "";
-    telefone: string = "";
+    nome: string;
+    telefone: string;
+
+    /**
+    *Método especial que executa apenas
+    *uma vez, que é ao criar o objeto
+    */
+    constructor( nome: string ) {
+        this.nome = nome;
+        this.telefone = "";
+    }
 }
--- Main.ts
+++ Main.ts
@@ -1,3 +1,14 @@
 import { Cliente } from "./Cliente";
 
-const cli: Cliente = new Cliente();
+const cli: Cliente = new Cliente("Gladimir");
+cli.telefone = "1234-5678";
+console.table(cli);
+//console.table faz o mesmo que o console.log, porém em formato de tabela.
+
+const cli2: Cliente = new Cliente("Edédio");
+
+const cli3: Cliente = cli;
+cli3.nome = "Bruna";
+console.table(cli);
+console.table(cli2);
+console.table(cli3);
