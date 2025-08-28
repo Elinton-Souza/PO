1d5308ff5d560d1835e4d378ba6fc307ad99d74f
elinton-souza <esc.vistorias@gmail.com>
2025-08-28 11:48:42 -0300

    Refatora diretórios e cria métodos como: cancelarPedido, adicionarItem, validarTelefone, alterarTelefone

--- SRC/Main.ts
+++ SRC/Main.ts
@@ -1,13 +1,12 @@
 import { Cliente } from "./Cliente";
 
-function criarCliente(nome: string, telefone: string): Cliente{
-    const cliente: Cliente = new Cliente();
-    cliente.criarCliente(nome, telefone);
-    return cliente;
+function criarCliente(nome: string, telefone: string): Cliente {
+  const cliente: Cliente = new Cliente();
+  cliente.criarCliente(nome, telefone);
+  return cliente;
 }
 
 const clientes: Cliente[] = [];
 clientes.push(criarCliente("Bruna", "123456789"));
 clientes.push(criarCliente("Edécio", "123456789"));
 clientes.push(criarCliente("Gladimir", "123456789"));
-
